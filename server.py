501256e0e428c228371b1bff809be0314a8a5084
Sha Jalal <106727351+ShajalalCSE@users.noreply.github.com>
2026-05-05 21:57:16 +0600

    Add basic TCP server functionality
    Implement a simple TCP server that accepts commands and sends them to a client.

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+s.bind(("127.0.0.1",8888))
+s.listen(1)
+print("Listening........")
+client , addr = s.accept()
+while True:
+    cmd = input("$ ")
+    client.send(cmd.encode())
+    if cmd == "exit":
+        break
+    output = (client.recv(1024)).decode()
+    print(output)
+client.close()
+s.close()
